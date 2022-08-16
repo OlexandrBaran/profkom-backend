80434ef7d5cff2b81a4e2549fc5bcb33b8fa5417
OlexandrBaran <olexandr.baran@gmail.com>
2022-08-16 20:43:15 +0300

    deploy

--- middleware/errorHandlingMiddleware.js
+++ middleware/errorHandlingMiddleware.js
@@ -1,7 +1,7 @@
-const ApiError = require('../error/apiError').default;
+//const ApiError = require('../error/apiError').default;
 
 module.exports = function(err, req, res, next) {
-    if(err instanceof ApiError){
+    if(err instanceof Error){
         return res.status(err.status).json({ message:err.message });
     }
     return res.status(500).json({ message:err.message});
