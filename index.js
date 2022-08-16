b3baa0eecc6417e82fdfbc98a325a5b0421f3213
OlexandrBaran <olexandr.baran@gmail.com>
2022-08-17 00:42:31 +0300

    deploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}));
 app.use('/', router);
 
@@ -25,6 +25,7 @@ if (process.env.NODE_ENV === "production") {
     
     app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    alert(__dirname)
   });
 }
 
