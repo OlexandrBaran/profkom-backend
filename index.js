627bc531822eed87a6b3b0fc12790e5b5dfe7621
OlexandrBaran <olexandr.baran@gmail.com>
2022-08-18 01:25:21 +0300

    deploy

--- index.js
+++ index.js
@@ -21,12 +21,11 @@ app.use(fileUpload({}));
 app.use('/', router);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'front/build/static')))
-    
+    const root = require('path').join(__dirname, 'client', 'build')
+    app.use(express.static(root));
     app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'front/build/index.html'));
-   
-  });
+        res.sendFile('index.html', { root });
+    })
 }
 
 
