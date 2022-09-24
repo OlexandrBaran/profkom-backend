f051c69c73b7923a728c0cb57385b21a928d152e
OlexandrBaran <olexandr.baran@gmail.com>
2022-09-24 19:31:28 +0300

    refactoring team members image

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(fileUpload({}));
 app.use('/', router);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'public')))
+    app.use(express.static(path.join(__dirname, 'static')))
     
     app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
